0ae45a6a1a02fe74e35d6c55ba846b5278c30ee5
Kartik Sarangmath <kartiksarangmath@gmail.com>
2020-03-25 23:37:37 -0500

    finished convolution backwards transformation

--- tf_verify/backwardprop.py
+++ tf_verify/backwardprop.py
@@ -132,7 +132,7 @@ def biasAddTerm(conds, termName, b):
 
         # this would be a place where turning caseConds into a map with var as key would be more efficient
 
-
+#all of the relu methods are not optimized to work together, loops through conds many more times than needed
 # comp is component of vectorVar to apply relu to
 def relu(conds, termName, var, comp):
     for i in range(len(conds)):
@@ -169,12 +169,43 @@ def reluLayerTerm(conds, termName):
         for i in range(layerSize):
             relu(conds, termName, var, i)
 
-# def conv2DLayerTerm(conds, termName, stride, kernelSize, filters, convFilters, convShape, inp, out):
-#     for cond in conds:
-#         for var in cond.termNameMap[termName].vars:
-#             W = np.zeros((out[0]*out[1]*out[2]*out[3],inp[0]*inp[1]*inp[2]))
-#             for i in range(convShape[-1]):
-#                 for j in range(convShape[-2]):
-#                     convMatrix = convFilters[:,:,j,i]
+def conv2DLayerTerm(stride, W, xdim=None, ydim=None, termName=None, conds=None):
+
+    for index in range(len(conds)):
+        cond = conds[index]
+        for var in cond.termNameMap[termName].vars:
+
+            reshapedW = np.zeros((ydim[1]*ydim[2]*ydim[3],xdim[0]*xdim[1]*xdim[2]))
+
+            for filter in range(ydim[3]):
+                linearConvs = np.zeros((xdim[2],((xdim[1] * (W.shape[0] - 1)) + W.shape[1])))
+                for i in range(W.shape[0]):
+                    for j in range(W.shape[1]):
+                        for k in range(W.shape[2]):
+                            linearConvs[k][(i * xdim[1]) + j] += W[i][j][k][filter]
+
+                offset = 0
+                resets = 0
+                for i in range(ydim[1] * ydim[2]):
+                    if offset + W.shape[1] > xdim[1]:
+                        resets += stride[0]
+                        offset = 0
+                    for j in range(W.shape[2]):
+                        reshapedW[filter + (i * ydim[3])][(resets * xdim[1]) + offset + (j * xdim[1]): (resets * xdim[1]) + offset + len(linearConvs[j]) + (j * xdim[1])] = linearConvs[j]
+                    offset += stride[1]
+
+            cond.termNameMap[termName].vars[var].W = cond.termNameMap[termName].vars[var].W.dot(reshapedW)
+            # cond.termNameMap[termName].vars[var].dim = cond.termNameMap[termName].vars[var].W.shape[0]
+            for i in range(len(cond.termNameMap[termName].caseConds)):
+                #this can be made more efficient with restructuring of caseConds
+                if cond.termNameMap[termName].caseConds[i][1] == var:
+                    cond.termNameMap[termName].caseConds[i][0] = cond.termNameMap[termName].caseConds[i][0].dot(reshapedW)
+
+def maxPoolLayerTerm():
+    
+
+
+
+
 
 
--- tf_verify/playground.py
+++ tf_verify/playground.py
@@ -14,7 +14,7 @@ translator = TFTranslator(model, False)  # second param indicates model is not o
 operations, resources = translator.translate()
 # print(operations)
 
-print(resources[4]['deeppoly'][0][:,:,15,0])
+# print(resources[4]['deeppoly'][0][0][0])
 
 # for i in range(len(operations)):5
 #     print(resources[i]['deeppoly'])
@@ -22,38 +22,47 @@ print(resources[4]['deeppoly'][0][:,:,15,0])
 #
 # print(operations[::-1])
 
+# W = np.array([[[[1,2]],[[0,5]]],[[[-1,2]],[[2,7]]]])
+# x = np.array([[[0],[4],[2],[1]],[[-1],[0],[1],[-2]],[[3],[1],[2],[0]],[[0],[1],[4],[1]]])
+# xdim=[4,4,1]
+# ydim=[1,2,2,2]
+# stride = [2,2]
+# conv2DLayerTerm(stride, W, xdim, ydim)
+# conv2DLayerTerm(resources[1]['deeppoly'][2], resources[1]['deeppoly'][0], resources[1]['deeppoly'][1], resources[1]['deeppoly'][-1])
+
+
 
 
 # -------------------------------------------------------------------------------------------------------------------
-conds = collections.deque([])
-y1 = VectorVar(name='y1', dim=2)
-y2 = VectorVar(name='y2', dim=2, cf=-1)
-term1 = Term(name='y', vars=[y1, y2], isAbs=True, cf=-1)
-x1 = VectorVar(name='x1', dim=1)
-x2 = VectorVar(name='x2', dim=1, cf=-1)
-term2 = Term(name='x', vars=[x1, x2], isAbs=True, cf='k')
-cond = Condition([term1, term2])
-conds.appendleft(cond)
-
-# conds contains the post condition of the entire NN
-
-print('Relu:')  # first backward transformation
-reluLayerTerm(conds, 'y')
-
-print('BiasAdd:')
-
-dummy = np.array([1, 3])
-biasAddTerm(conds, 'y', dummy)
-# biasAddTerm(conds, 'y', resources[-2]['deeppoly'][0])
-# print(condsToString(conds))
-# print(len(conds))
-
-print('Matmul:')
-dummy = np.array([[4, 2], [2, 9]])
-matmulTerm(conds, 'y', dummy)
-# matmulTerm(conds, 'y', resources[-3]['deeppoly'][0])
-print('DONE')
-# condsToString(conds)
-# print(len(conds))
-
-# note that the variables in the cases represent y_i = W_i * y_i + b_i
+# conds = collections.deque([])
+# y1 = VectorVar(name='y1', dim=2)
+# y2 = VectorVar(name='y2', dim=2, cf=-1)
+# term1 = Term(name='y', vars=[y1, y2], isAbs=True, cf=-1)
+# x1 = VectorVar(name='x1', dim=1)
+# x2 = VectorVar(name='x2', dim=1, cf=-1)
+# term2 = Term(name='x', vars=[x1, x2], isAbs=True, cf='k')
+# cond = Condition([term1, term2])
+# conds.appendleft(cond)
+#
+# # conds contains the post condition of the entire NN
+#
+# print('Relu:')  # first backward transformation
+# reluLayerTerm(conds, 'y')
+#
+# print('BiasAdd:')
+#
+# dummy = np.array([1, 3])
+# biasAddTerm(conds, 'y', dummy)
+# # biasAddTerm(conds, 'y', resources[-2]['deeppoly'][0])
+# # print(condsToString(conds))
+# # print(len(conds))
+#
+# print('Matmul:')
+# dummy = np.array([[4, 2], [2, 9]])
+# matmulTerm(conds, 'y', dummy)
+# # matmulTerm(conds, 'y', resources[-3]['deeppoly'][0])
+# print('DONE')
+# # condsToString(conds)
+# # print(len(conds))
+#
+# # note that the variables in the cases represent y_i = W_i * y_i + b_i
